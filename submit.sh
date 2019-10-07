4ffca078d2095821da8a98dc0c7db9638f4ce6be
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2019-10-07 18:12:52 -0400

    Update

--- submit.sh
+++ submit.sh
@@ -23,6 +23,20 @@ then
     exit 1
 fi
 
+rm -rf Velopipe.deps.zip
+zip Velopipe.deps.zip modules modules/*
+
+# $ unzip -l Velopipe.deps.zip
+# Archive:  Velopipe.deps.zip
+#   Length      Date    Time    Name
+# ---------  ---------- -----   ----
+#         0  10-04-2019 10:37   modules/
+#       762  10-04-2019 13:42   modules/SortIndexBam.wdl
+#       848  10-04-2019 10:27   modules/TagBam.wdl
+#       947  10-07-2019 10:29   modules/Velocyto.wdl
+# ---------                     -------
+#      2557                     4 files
+
 cromwell-tools submit \
     --secrets-file ${service_account_key} \
     --wdl Velopipe.wdl \
--- velopipe-palantir.sh
+++ velopipe-palantir.sh
@@ -37,6 +37,20 @@ cat > ${path_label} <<FILE
 }
 FILE
 
+rm -rf Velopipe.deps.zip
+zip Velopipe.deps.zip modules modules/*
+
+# $ unzip -l Velopipe.deps.zip
+# Archive:  Velopipe.deps.zip
+#   Length      Date    Time    Name
+# ---------  ---------- -----   ----
+#         0  10-04-2019 10:37   modules/
+#       762  10-04-2019 13:42   modules/SortIndexBam.wdl
+#       848  10-04-2019 10:27   modules/TagBam.wdl
+#       947  10-07-2019 10:29   modules/Velocyto.wdl
+# ---------                     -------
+#      2557                     4 files
+
 # subtmit
 cromwell-tools submit \
     --secrets-file ${service_account_key} \

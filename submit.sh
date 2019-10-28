c7c850b97377c3729dbb0c8fcca4a51b521a773b
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2019-10-28 16:12:33 -0400

    Support inputs, lables, and options

--- submit.sh
+++ submit.sh
@@ -5,19 +5,25 @@ usage()
 cat << EOF
 USAGE: `basename $0` [options]
     -k  service account key (e.g. secrets.json)
+    -i  inputs file (e.g. sample.inputs.json)
+    -l  labels file (e.g. labels.json)
+    -o  options file (e.g. options.json)
 EOF
 }
 
-while getopts "k:h" OPTION
+while getopts "k:i:l:o:h" OPTION
 do
     case $OPTION in
         k) service_account_key=$OPTARG ;;
+        i) inputs_file=$OPTARG ;;
+        l) labels_file=$OPTARG ;;
+        o) options_file=$OPTARG ;;
         h) usage; exit 1 ;;
         *) usage; exit 1 ;;
     esac
 done
 
-if [ -z "$service_account_key" ]
+if [ -z "$service_account_key" ] || [ -z "$inputs_file" ] || [ -z "$labels_file" ] || [ -z "$options_file" ]
 then
     usage
     exit 1
@@ -37,8 +43,11 @@ zip Velopipe.deps.zip modules modules/*
 # ---------                     -------
 #      2557                     4 files
 
+
 cromwell-tools submit \
     --secrets-file ${service_account_key} \
     --wdl Velopipe.wdl \
-    --inputs-files Velopipe.inputs.json \
-    --deps-file Velopipe.deps.zip    
+    --deps-file Velopipe.deps.zip \
+    --inputs-files ${inputs_file} \
+    --label-file ${labels_file} \
+    --options-file ${options_file}

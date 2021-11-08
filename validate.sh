09b6bbb7e3241104b3aea17ca0f1f4a2e6097ee4
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2021-11-08 12:17:13 -0500

    Remove hardcoded womtool.jar path

--- validate.sh
+++ validate.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-java -jar ~/Applications/womtool.jar \
+if [ -z $SCING_HOME ]
+then
+    echo "Environment variable 'SCING_HOME' not defined."
+    exit 1
+fi
+
+java -jar ${SCING_HOME}/devtools/womtool.jar \
     validate \
     Velopipe2.wdl \
-    --inputs ./config/test2.inputs.aws.json
+    --inputs ./config/template.inputs.json

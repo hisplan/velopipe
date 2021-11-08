74084d2b1c7e36157e11c054ec6bb6e50f000e53
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2021-11-08 12:15:10 -0500

    Set version to 0.0.8

--- make-deployable.sh
+++ make-deployable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 wf_name="velopipe"
-version="0.0.7"
+version="0.0.8"
 files="submit.sh Velopipe2.wdl Velopipe.deps.zip Velopipe.options.aws.json config/template.*.json"
 dest="$HOME/scing/bin"
 
